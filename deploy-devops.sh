d748a787cf0b0117712ca1f75f9afceb565863d4
kanghouchao <hchkang8710@gmail.com>
2019-11-04 23:27:01 +0800

    修改发布

--- deploy-devops.sh
+++ deploy-devops.sh
@@ -1,8 +1,5 @@
-docker rmi -f 192.168.173.91:8082/repository/container/client:latest
-docker build -t 192.168.173.91:8082/repository/container/client:latest /home/vin/IdeaProjects/devops/devops-client/devops-client-web
-docker push 192.168.173.91:8082/repository/container/client:latest
-docker rmi -f 192.168.173.91:8082/repository/container/server:latest
-docker build -t 192.168.173.91:8082/repository/container/server:latest /home/vin/IdeaProjects/devops/devops-server/devops-server-web
-docker push 192.168.173.91:8082/repository/container/server:latest
+/#! /bin/bash
+docker stack rm nacos
 docker stack rm devops
-docker stack deploy --with-registry-auth -c docker-compose.yml devops
+docker stack deploy --with-registry-auth -c nacos-compose.yml nacos
+docker stack deploy --with-registry-auth -c devops-compose.yml devops
