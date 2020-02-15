74163f6b0140870fda325ff0c890c595ad10e72e
kanghouchao <hchkang8710@gmail.com>
2020-02-15 19:03:28 +0800

    添加删除脚本

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+rm -rf ./nginx/logs
+rm -rf ./standalone-logs
 mkdir -p ./nginx/logs
 mkdir -p ./standalone-logs
-sudo docker network create --subnet 10.0.9.0/24 --gateway 10.0.9.1 --scope swarm --driver overlay devops_net
+docker network create --subnet 10.0.9.0/24 --gateway 10.0.9.1 --scope swarm --driver overlay devops_net
