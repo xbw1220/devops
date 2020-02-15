29112a76c0916c348763fdc00dad07d85a1181d5
kanghouchao <hchkang8710@gmail.com>
2020-02-15 19:36:59 +0800

    添加删除docker网络语句

--- init.sh
+++ init.sh
@@ -2,4 +2,5 @@ rm -rf ./nginx/logs
 rm -rf ./standalone-logs
 mkdir -p ./nginx/logs
 mkdir -p ./standalone-logs
+docker network remove devops_net
 docker network create --subnet 10.0.9.0/24 --gateway 10.0.9.1 --scope swarm --driver overlay devops_net
