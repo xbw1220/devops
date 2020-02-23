e28bffa247e243ddb473935ae881b7acdc7aa401
kanghouchao <hchkang8710@gmail.com>
2020-02-23 19:54:26 +0800

    修改token-server所使用的镜像为自建镜像

--- build-push.sh
+++ build-push.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 /usr/local/maven/bin/mvn clean install -e -am -B -Dmaven.test.skip=true -pl devops-server/devops-server-api
-/usr/local/maven/bin/mvn clean package -e -B -Dmaven.test.skip=true -pl devops-server/devops-server-web,devops-client/devops-client-web,devops-gateway
+/usr/local/maven/bin/mvn clean package -e -B -Dmaven.test.skip=true -pl devops-server/devops-server-web,devops-client/devops-client-web,devops-gateway,sentinel-cluster-server
 docker rmi -f 192.168.173.91:8082/repository/container/server:latest
 docker rmi -f 192.168.173.91:8082/repository/container/client:latest
 docker rmi -f 192.168.173.91:8082/repository/container/gateway:latest
+docker rmi -f 192.168.173.91:8082/repository/container/sentinel-cluster-server:latest
 cd ~/IdeaProjects/devops/devops-server/devops-server-web && docker build -t 192.168.173.91:8082/repository/container/server .
 cd ~/IdeaProjects/devops/devops-client/devops-client-web && docker build -t 192.168.173.91:8082/repository/container/client .
 cd ~/IdeaProjects/devops/devops-gateway && docker build -t 192.168.173.91:8082/repository/container/gateway .
+cd ~/IdeaProjects/devops/sentinel-cluster-server && docker build -t 192.168.173.91:8082/repository/container/sentinel-cluster-server .
 docker login http://192.168.173.91:8082
 docker push 192.168.173.91:8082/repository/container/server:latest
 docker push 192.168.173.91:8082/repository/container/client:latest
 docker push 192.168.173.91:8082/repository/container/gateway:latest
+docker push 192.168.173.91:8082/repository/container/sentinel-cluster-server:latest
