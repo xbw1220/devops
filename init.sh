dfe2b43d46a566a76fb46b7c99b5e0f28a3290f4
kanghouchao <hchkang8710@gmail.com>
2020-02-23 15:11:35 +0800

    修改脚本

--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
 rm -rf ./nginx/logs
+rm -rf ./standalone-logs
 mkdir -p ./nginx/logs
+mkdir -p ./standalone-logs
+docker system prune
 docker network remove devops_net
 docker network create --subnet 10.0.9.0/24 --gateway 10.0.9.1 --scope swarm --driver overlay devops_net
